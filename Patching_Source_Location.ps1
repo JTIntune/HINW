f4fb5fc7a04ef3f319d666f5f70a2319327b914f
JTIntune <Jyotish.php@gmail.com>
2025-04-28 19:04:32 +0530

    Add files via upload

---
+++ Patching_Source_Location.ps1
@@ -0,0 +1,8 @@
+﻿
+$MUSM = New-Object -ComObject "Microsoft.Update.ServiceManager"
+
+$Output = $MUSM.Services | Where-Object { $_.IsDefaultAUService -eq $true }
+
+$Patching_Source_Location = if ($Output.Name -eq "Windows Server Update Service" ) {"SCCM"} ElseIf ($Output.Name -eq "Windows Update" ) {"Windows Update"} ElseIf ($Output.Name -eq "Microsoft Update" ) {"Intune/GPO"} Else {$Output.Name }
+
+$Patching_Source_Location
